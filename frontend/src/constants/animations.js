1faa64e01039124c3ca3a5563168f6b46b514a88
Zeyad Ahmed Mowafe <zezomowafe2@gmail.com>
2025-10-29 03:43:38 +0300

    Update animations.js

--- frontend/src/constants/animations.js
+++ frontend/src/constants/animations.js
@@ -1,25 +1,21 @@
 // pageTransitions.js
-
 export const pageVariants = {
   initial: {
     opacity: 0,
-    y: 20, // بدل x خليها y بسيطة لتدي إحساس ارتفاع ناعم
-    scale: 0.98,
+    scale: 0.95,
   },
   in: {
     opacity: 1,
-    y: 0,
     scale: 1,
   },
   out: {
     opacity: 0,
-    y: -15,
     scale: 1.02,
   },
 };
 
 export const pageTransition = {
   type: "tween",
-  ease: "easeInOut",
-  duration: 0.2,
+  ease: [0.25, 0.1, 0.25, 1], // cubic-bezier للسلاسة
+  duration: 0.3,
 };
